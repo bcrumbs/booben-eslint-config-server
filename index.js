e2c16ca3975e38ef8710a37022f365e9a7e29940
Dmitriy Bizyaev <bizyaev@ordbuy.com>
2017-01-11 16:32:14 +0300

    added comma-dangle rule

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ module.exports = {
   'rules': {
     'global-require': [0],
 
-    'import/no-dynamic-require': [0]
+    'import/no-dynamic-require': [0],
+  
+    // Added by Dmitriy Bizyaev on 11.01.2017
+    'comma-dangle': [2, {
+      'arrays': 'always-multiline',
+      'objects': 'always-multiline',
+      'imports': 'always-multiline',
+      'exports': 'always-multiline',
+      'functions': 'never',
+    }]
   },
   'env': {
     'node': true
